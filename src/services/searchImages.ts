1dc69b397349bfa02a4e41b18db529a5711728da
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2022-11-25 23:54:12 -0300

    chore: add page to request

--- src/services/searchImages.ts
+++ src/services/searchImages.ts
@@ -1,9 +1,10 @@
 import api from './api';
 import Config from 'react-native-config';
 
-export const getImages = async (text: string) => {
+export const getImages = async (text: string, page?: number) => {
   try {
-    const response = await api.get(`/api/?key=${Config.PIXABAY_KEY}&q=${encodeURIComponent(text)}`);
+    const response = await api
+      .get(`/api/?key=${Config.PIXABAY_KEY}&q=${encodeURIComponent(text)}${page && '&page=' + String(page)}`);
     return response; 
   } catch (err) {
   }
